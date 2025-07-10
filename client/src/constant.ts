4b1e2996855ae92c9c555c38849eb483b857c02e
Abhijeet Kumar <abhijeetthakur7080@gmail.com>
2025-07-11 01:10:08 +0530

    some server update

--- client/src/constant.ts
+++ client/src/constant.ts
@@ -1,1 +1,2 @@
-export const baseUrl = import.meta.env.NODE_ENV==="production" ? "/api ":"http://localhost:5000/api"
+// export const baseUrl = import.meta.env.NODE_ENV==="production" ? "/api":"http://localhost:5000/api"
+export const baseUrl = "/api"
